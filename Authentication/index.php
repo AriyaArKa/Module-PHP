4b2811707810a5d450fcc1549722ebed747f9afb
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-03-09 01:24:06 +0600

    first commit

---
+++ Authentication/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//header
+include 'includes/header.php';
+
+//navbar
+include 'includes/navbar.php';
+
+//footer
+include 'includes/footer.php';
---
+++ Authentication/login.php
@@ -0,0 +1,45 @@
+
+<?php
+
+$pageTitle = "Login Page";
+
+//header
+include 'includes/header.php';
+
+//navbar
+include 'includes/navbar.php';
+?>
+
+
+
+    <!-- Login form -->
+    <div class="row" mt-5>
+        <div class="col-6 offset-md-3">
+
+            <div class="alert alert-success my-2" role="alert">
+                Login Success
+            </div>
+
+            <div class="alert alert-danger my-2" role="alert">
+                Invalid Credentials
+            </div>
+
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
+
+
+<?php
+//footer
+include 'includes/footer.php';
+?>
