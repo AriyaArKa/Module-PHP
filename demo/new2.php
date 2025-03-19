f7fda5f1bdd813c2fbc8b8cf92865dec8e87d49e
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-03-20 00:01:54 +0600

    string proerties

--- demo/new2.php
+++ demo/new2.php
@@ -1,3 +1,44 @@
 <?php
 
+// <!-- php new2.php -->
+
+
+$string = 'arka braja prasad nath';
+
+
+//string length
+echo strlen($string);
+
+echo "\n";
+
+//reverse
+echo strrev($string);
+
+echo "\n";
+
+//string to lower
+echo strtolower($string);
+
+echo "\n";
+
+//string to upper
+echo strtoupper($string);
+
+
+echo "\n";
+
+$string = "hlw how are you?";
+
+
+//first letter capital
+echo ucfirst($string);
+
+
+echo "\n";
+
+
+//first letter of word capital
+echo ucwords($string);
+
+echo "\n";
 
