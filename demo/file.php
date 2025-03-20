db615da8e02ab9ac6cd57464f4851167e814c3e5
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-03-20 22:04:16 +0600

    updated

--- demo/file.php
+++ demo/file.php
@@ -2,10 +2,28 @@
 
 $file_name = 'test.txt';
 
-$file = fopen($file_name,'r') or die('Unable  to pen');
+// $file = fopen($file_name,'r') or die('Unable  to pen');
 
-echo 'File opened successfully';
+// echo 'File opened successfully';
 
-// Reading from a file
+// // Reading from a file
 
-echo fread($file,filesize($file_name));
+// //echo fread($file,filesize($file_name));
+
+// while (!feof($file)) {
+//     echo"<h3>".fgets($file)."</h3>";
+// }
+$file = fopen($file_name,'w') or die('Unable  to pen');
+
+$new_text = "This is our new text";
+
+fwrite($file,$new_text);
+
+//fclose($file);
+
+if(unlink($file_name)){
+    echo "File deleted";
+}else{
+    echo "file not deleted";
+}
+
