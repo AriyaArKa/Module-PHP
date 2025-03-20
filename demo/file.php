9b9967b030bd47438df3d4a8db4b90db84f637e2
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-03-20 21:48:40 +0600

    read properly

--- demo/file.php
+++ demo/file.php
@@ -1,3 +1,11 @@
 <?php
 
-$file = fopen('','r'); 
+$file_name = 'test.txt';
+
+$file = fopen($file_name,'r') or die('Unable  to pen');
+
+echo 'File opened successfully';
+
+// Reading from a file
+
+echo fread($file,filesize($file_name));
